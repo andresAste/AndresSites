a3ab4e6292bc57b2dac3695706429efb36583600
Andrés Asteasuain <andres.asteasuain@inallmedia.com>
2016-12-11 10:51:54 -0300

    creando index para componentes

---
+++ app/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./calendario.component'));
+__export(require('./compras.component'));
+__export(require('./deudasMes.component'));
+__export(require('./editarPago.component'));
+__export(require('./home.component'));
+__export(require('./app.component'));
+//# sourceMappingURL=index.js.map
---
+++ app/index.ts
@@ -0,0 +1,6 @@
+export * from './calendario.component';
+export * from './compras.component';
+export * from './deudasMes.component';
+export * from './editarPago.component';
+export * from './home.component';
+export * from './app.component';
